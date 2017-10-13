12d59fc5f765003566b6ce289f31246b83981995
paulbearne <paulbearne04@gmail.com>
2017-10-13 08:14:15 +0100

    Add project files.

---
+++ WiFiLib/WiFiLib.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		WiFiLib.cpp
+ Created:	10/13/2017 8:14:09 AM
+ Author:	paul
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "WiFiLib.h"
+
+
---
+++ WiFiLib/WiFiLib.h
@@ -0,0 +1,19 @@
+/*
+ Name:		WiFiLib.h
+ Created:	10/13/2017 8:14:09 AM
+ Author:	paul
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _WiFiLib_h
+#define _WiFiLib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
